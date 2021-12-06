170a0dfb5828716d2b21d2655acd609cdd40c948
Alysu <alyson.subrim2002@gmail.com>
2021-12-06 02:29:25 -0300

    Adiciona classes de cadastro e esqueci_senha, altera cor de acentuação(accent)

---
+++ app/src/main/java/com/example/appsalaobeleza1/CadastraFunc.java
@@ -0,0 +1,14 @@
+package com.example.appsalaobeleza1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastraFunc extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastra_func);
+    }
+}
